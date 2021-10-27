591df317c9774705410ae575e0ad78582bbece2c
RohanFredriksson <rfre3327@uni.sydney.edu.au>
2021-10-27 18:46:19 +1100

    changed command arguments to be more user friendly

--- main.py
+++ main.py
@@ -9,34 +9,38 @@ def main():
     
     args = sys.argv
     if len(args) < 7:
-        print("Not enough arguments provided.\nExample usage: python3 main.py width height iterations r_max c_max r_min c_min [filename]")
+        print("Not enough arguments provided.\n\nUsage:   python3 main.py x y zoom width height iterations [filename]\nExample: python3 main.py -0.75 0 1 1920 1080 64\n")
         return
 
+    x = 0
+    y = 0
+    zoom = 0
     width = 0
     height = 0
     iterations = 0
-    r_max = 0.0
-    c_max = 0.0
-    r_min = 0.0
-    c_min = 0.0
 
     try:
-        width = int(args[1])
-        height = int(args[2])
-        iterations = int(args[3])
-
-        r_max = float(args[4])
-        c_max = float(args[5])
-
-        r_min = float(args[6])
-        c_min = float(args[7])
+        x = float(args[1])
+        y = float(args[2])
+        zoom = float(args[3])
+        width = int(args[4])
+        height = int(args[5])
+        iterations = int(args[6])
     except:
         print("Error: One of the numeric arguments is not numerical.")
         return
 
     filename = "fractal.png"
-    if len(args) > 8:
-        filename = args[8]
+    if len(args) > 7:
+        filename = args[7]
+
+    units_per_pixel = 5 / (zoom * width)
+
+    r_max = x + (width / 2) * units_per_pixel
+    c_max = y + (height / 2) * units_per_pixel
+
+    r_min = x - (width / 2) * units_per_pixel
+    c_min = y - (height / 2) * units_per_pixel
 
     r_delta = (r_max - r_min) / width
     c_delta = (c_max - c_min) / height
